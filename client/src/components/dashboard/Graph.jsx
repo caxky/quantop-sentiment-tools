f963549b2146e3ce744fd3573f1caa027dae985c
caxky <k_james7@outlook.com>
2022-06-30 03:20:48 -0400

    Time scale selectors for graphs

--- client/src/components/dashboard/Graph.jsx
+++ client/src/components/dashboard/Graph.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { Chart } from 'primereact/chart';
+import { ToggleButton, ToggleButtonGroup, Box } from '@mui/material';
 
 export default function Table(props) {
+
+  const [alignment, setAlignment] = React.useState('24h');
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
 
   const [graphData] = useState({
     labels: props.labels,
@@ -29,6 +36,30 @@ export default function Table(props) {
   }
 
   return (
-    <Chart type="bar" data={graphData} options={graphOptions}/>
+    <div className='graph'>
+      <Chart type="bar" data={graphData} options={graphOptions}/>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          '& > *': {
+            m: 1,
+          },
+        }}
+      >
+        <ToggleButtonGroup
+          color="primary"
+          value={alignment}
+          exclusive
+          onChange={handleChange}
+        >
+          <ToggleButton value="24h">24h</ToggleButton>
+          <ToggleButton value="7d">7d</ToggleButton>
+          <ToggleButton value="30d">30d</ToggleButton>
+        </ToggleButtonGroup>
+      </Box>
+    </div>
+    
   )
 }
