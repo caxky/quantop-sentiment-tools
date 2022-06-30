5f6eac0a9e8e644b2329a3be935870fe76764ea3
caxky <k_james7@outlook.com>
2022-06-30 04:23:58 -0400

    Finish content on home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.scss';
+import { Col, Row } from 'react-bootstrap';
 
 export default function Home() {
   return (
@@ -8,44 +9,73 @@ export default function Home() {
         <div className='home-banner-logo'>
           <img src='/images/logo.svg' alt='logo' />
         </div>
-        <h1>Sentiment Analysis Tools</h1>
+        <h2>Sentiment Analysis Tools</h2>
       </div>
 
       <div className='tool-summary'>
         <div className='tool-summary-item'>
-          Item summary
+          <p>
+            Stock Sentiment Analysis,
+            Cryptocurrency Analysis,
+            Learning
+          </p>
         </div>
       </div>
 
       <div className='sections'>
-        <div className='section'>
+        <Col className='section' md={8}>
           <div className='section-title'>
             <h5>Stock Analysis</h5>
           </div>
-          <div className='section-content'>
-            <img src='/images/stock-graph.jpg' alt='stock graph'></img>
-            <p>Breakdown what the user will find here</p>
-          </div>
-        </div>
-        <div className='section'>
+          <Row className='section-content'>
+            <Col md={4}>
+              <img src='/images/stock-graph.jpg' alt='stock graph'></img>
+            </Col>
+            <Col md={8}>
+              <ul className='breakdown-list'>
+                <li>Current price and sentiment data</li>
+                <li>Most positive/most negative sentiment graphs</li>
+                <li>Time scales</li>
+                <li>Data table for thousands of companies</li>
+              </ul>
+            </Col>
+          </Row>
+        </Col>
+        <Col className='section' md={8}>
           <div className='section-title'>
-            <h5>Crypto Analysis</h5>
+            <h5>Cryptocurrency Analysis</h5>
           </div>
-          <div className='section-content'>
-            <img src='/images/crypto-image.jpg' alt='bitcoin on circuit'></img>
-            <p>Breakdown what the user will find here</p>
-          </div>
-        </div>
-
-        <div className='section'>
+          <Row className='section-content'>
+            <Col md={4}>
+              <img src='/images/crypto-image.jpg' alt='bitcoin on circuit'></img>
+            </Col>
+            <Col md={8}>
+              <ul className='breakdown-list'>
+                <li>Current price and sentiment data</li>
+                <li>Most positive/most negative sentiment graphs</li>
+                <li>Time scales</li>
+                <li>Data table for hundreds of currencies</li>
+              </ul>
+            </Col>
+          </Row>
+        </Col>
+        <Col className='section' md={8}>
           <div className='section-title'>
             <h5>Learn</h5>
           </div>
-          <div className='section-content'>
-            <img src='/images/books.jpg' alt='books'></img>
-            <p>Breakdown what the user will find here</p>
-          </div>
-        </div>
+          <Row className='section-content'>
+            <Col md={4}>
+              <img src='/images/books.jpg' alt='books'></img>
+            </Col>
+            <Col md={8}>
+              <ul className='breakdown-list'>
+                <li>Technical analysis</li>
+                <li>Fundamental analysis</li>
+                <li>Common terms</li>
+              </ul>
+            </Col>
+          </Row>
+        </Col>
       </div>
     </div>
   )
